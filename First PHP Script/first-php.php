ce8b69025b65b92d61787e1b590729145e6376d2
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-20 09:56:21 +0530

    make PHP file and echo string and combine string using '.', and also use // /**/ and # for comment php codes

---
+++ First PHP Script/first-php.php
@@ -0,0 +1,23 @@
+<?php 
+    echo "This is my first PHP.";
+    echo "<br>";
+    echo "This is my first PHP.";
+
+    echo "<br>";
+    echo "Sandun" . " " . "Senevirathna";
+
+    echo "<br>";
+    echo 2500;
+
+    echo "<br>";
+    echo 2500 / 100;
+
+    //this is single line comment
+
+    /**
+     * this is multi line comment
+     * see I can comment more
+     */
+
+     #this is another single line comment
+?>
