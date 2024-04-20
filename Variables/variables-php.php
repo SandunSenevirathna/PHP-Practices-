8b27326ac7c570e483667282ff2131cbeed99053
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-20 11:08:16 +0530

    create variables example

---
+++ Variables/variables-php.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Variables";
+    echo "<br>";
+    echo "<br>";
+
+    $firstName = "Sandun";
+    $lastName = "Senevirathna";
+    $age = 25;
+
+    echo $firstName . " " . $lastName;
+    echo "<br>";
+    echo $age;
+
+    $firstName = "Kapila";
+
+    echo "<br>";
+    
+    echo $firstName . " " . $lastName;
+
+
+?>
