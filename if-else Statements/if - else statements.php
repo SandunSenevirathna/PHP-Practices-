19124809f6b4aef665fe947cdfebbfdaa604b65c
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-22 09:07:24 +0530

    if else

---
+++ if-else Statements/if - else statements.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP if | else</title>
+</head>
+
+<body>
+
+    <h1>PHP if | else</h1>
+    <br>
+
+    <?php
+        
+        $number = 11;
+
+    ?>
+
+   IF and ELSE 
+   <br>
+    <?php 
+         
+         if($number <= 10){
+            echo "inside of the if";
+         }
+         else{
+            echo "inside of the else";
+         }
+    
+    ?>
+
+
+</body>
+
+</html>
